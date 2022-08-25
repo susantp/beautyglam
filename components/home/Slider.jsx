9807f24a1b078c575a412cc1b77c8af7bc157658
Susant Paudel <techbizznepal@gmail.com>
2022-08-25 20:44:02 +0545

    ...

--- components/home/Slider.jsx
+++ components/home/Slider.jsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 const Slider = ({heading, subHeading, image}) => {
     return (
         <div className={`bg-neutral-800`}>
-            <div className={`container mx-auto flex justify-between items-center`}>
+            <div className={`container mx-auto flex justify-between items-center flex-wrap`}>
                 <div className={`flex flex-col gap-y-10 px-20 py-40`}>
-                    <p className={`font-thin text-3xl uppercase text-white`}>{subHeading}</p>
-                    <p className={`text-white text-orange-600 font-lobster text-8xl `}>{heading}</p>
+                    <p className={`font-thin lg:text-3xl md:text-3xl sm:text-2xl uppercase text-white`}>{subHeading}</p>
+                    <p className={`text-white text-orange-600 font-lobster text-4xl sm:text-4xl md:text-6xl lg:text-8xl  `}>{heading}</p>
                     <p className={`font-thin text-white text-2xl`}>Beauty service that leave you looking & feeling your best.</p>
                 </div>
                 <div>
--- components/home/WhatWeDoComponent.jsx
+++ components/home/WhatWeDoComponent.jsx
@@ -37,7 +37,7 @@ const WhatWeDoComponent = ({heading,subHeading, items}) => {
                             key={item.slug}/>
                     )}
                 </div>
-                <div className={`flex justify-evenly space-x-10 invisible md:visible`}>
+                <div className={`flex justify-evenly space-x-10 `}>
                     <div className={`text-2xl flex p-4 bg-orange-600 rounded-lg text-white items-center cursor-pointer select-none`}>
                         <p className={`uppercase`}>
                             <Link href={`/appointment`}>
